0368ba62923d1147c93379f061087ced1146e2af
Andrei Dzeichyk <Andrei_Dzeichyk@epam.com>
2018-08-14 10:53:52 +0300

    A new function has been added to help highlight element when bedugging

---
+++ step_definitions/utils/stepFunctions.js
@@ -0,0 +1,42 @@
+const logger = require('../../configs/winstone.conf').logger;
+
+// TODO merge these two functions
+
+const highlightElement = (element) => {
+
+    let bg;
+    return element.getCssValue('backgroundColor').then((col) => {
+        bg = col;
+    }).then(() => {
+        return browser.executeScript('arguments[0].style.backgroundColor = "red"', element)
+    }).then(() => {
+        return browser.driver.sleep(1000);
+    }).then(() => {
+        return browser.executeScript(`arguments[0].style.backgroundColor = "${bg}"`, element)
+    }).then(() => {
+        return browser.driver.sleep(1000);
+    }).catch(err => {
+        logger.error('Error is: ' + err);
+        throw new Error(err)
+    });
+};
+
+const highlightElement2 = (element) => {
+
+    const styleOptions = 'color: Red; border: 2px solid red;';
+    return browser.executeScript('arguments[0].setAttribute("style", arguments[1]);', element, styleOptions).then(() => {
+        return browser.wait(() => {
+            return element.getCssValue('border').then((border) => {
+                return border.toString().indexOf('2px solid rgb(255,') > -1;
+                });
+            }, 
+            TIMEOUT.m,
+            'Style is not applied!'
+        );
+    }, (err) => {
+        logger.error('Error is: ' + err);
+        throw new Error(err);
+    });
+};
+
+module.exports = {highlightElement};
